57e75a28ad38767e590f4dec150277a836642dc5
Josh Mu <hello@joshmu.dev>
2022-04-03 14:08:47 +1000

    intl price formatter

--- src/components/TotalUSD.tsx
+++ src/components/TotalUSD.tsx
@@ -1,6 +1,5 @@
 import { FC, useMemo } from 'react'
 import { useAppContext } from '../context/globalContext'
-import CryptoIcon from './cryptoIcon'
 
 function getTotalUSDValue({
   wallet,
@@ -18,6 +17,16 @@ function getTotalUSDValue({
 
   return btcPrice * btcAmount + ethPrice * ethAmount + usdAmount
 }
+
+// Create our number formatter.
+const priceUSDFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+
+  // These options are needed to round to whole numbers if that's what you want.
+  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+})
 
 export const TotalUSD: FC = () => {
   const { wallet, prices } = useAppContext()
@@ -47,8 +56,7 @@ export const TotalUSD: FC = () => {
         alignItems: 'center',
       }}
     >
-      <CryptoIcon className='symbol' assetName='usd' />
-      {totalUSDValue.toFixed(2)}
+      {priceUSDFormatter.format(totalUSDValue)}
     </span>
   )
 }
